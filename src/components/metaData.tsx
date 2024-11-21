c62e52c606e9550aba991f7f61a0f9167a973b3b
Andreas Wolf <andreas.wolf@diva-e.com>
2024-11-21 01:42:00 +0100

    adds Metadata improvement

--- src/components/metaData.tsx
+++ src/components/metaData.tsx
@@ -4,39 +4,6 @@ import { Helmet } from "react-helmet";
 const MetaData: React.FC<{ color: string }> = (props) => {
   return (
     <Helmet>
-      <meta
-        name="description"
-        content="Weighted Voronoi Stippling. It stipples."
-      ></meta>
-      <meta
-        property="og:title"
-        content="Stippler - Weighted Voronoi Stippling"
-      ></meta>
-      <meta
-        property="og:description"
-        content="Weighted Voronoi Stippling. It stipples."
-      />
-      <meta
-        property="og:image"
-        content="https://stippler.wolfandreas.com/screenshot.png"
-      />
-      <meta
-        name="keywords"
-        content="Stippler, Voronoi, Stippling, Weighted Voronoi, Weighted Voronoi Stippling"
-      ></meta>
-
-      <script type="application/ld+json">
-        {`
-                    {
-                        "@context": "https://schema.org",
-                        "@type": "WebSite",
-                        "url": "https://stippler.wolfandreas.com/",
-                        "name": "Stippler",
-                        "image": "https://stippler.wolfandreas.com/screenshot.png",
-                        "abstract": "Weighted Voronoi Stippling. It stipples."
-                    }
-                `}
-      </script>
       <meta name="theme-color" content={props.color}></meta>
       <style>
         {`
